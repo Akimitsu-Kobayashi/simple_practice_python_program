e7d47846acc31e7aa4beda23d3addeacee6287bb
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-21 13:56:17 +0800

    Strips leading spaces of string

--- remove_beginning_spaces.py
+++ remove_beginning_spaces.py
@@ -1,1 +1,5 @@
 #Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+
+name = input("Enter your fullname: ").strip()
+
+print(f"Your fullname is: {name}")
