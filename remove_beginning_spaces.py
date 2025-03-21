5fa30c2ab6df3da53d7479620f2097bb4750d18a
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-21 17:05:02 +0800

    added a few notes

--- remove_beginning_spaces.py
+++ remove_beginning_spaces.py
@@ -1,5 +1,7 @@
 #Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
 
-name = input("Enter your fullname: ").strip()
+# get the users full name and remove leading and 
+name = input("Enter your full name: ").strip()
 
-print(f"Your fullname is: {name}")
+# print the formatted full name
+print(f"Your full name is: {name}")
