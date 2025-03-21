2c0f62c391eb1e957f92e4937b841cd7cdf7b5f2
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-21 14:34:45 +0800

    lowercase string

---
+++ lowercase_string.py
@@ -0,0 +1,7 @@
+#Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
+
+#lowercase the string inputted 
+name = input("Enter your full name: ").lower()
+
+#print the formatted full name
+print(f"Your full name in lowercase is: {name}")
