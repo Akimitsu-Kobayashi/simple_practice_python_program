79c4a580402852ecd1ec001b17fc4d4ed43198e0
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-21 14:45:00 +0800

    swaps casing of string

---
+++ swap_case_of_string.py
@@ -0,0 +1,7 @@
+#Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
+#swaps the casing of string
+name = input("Enter your full name: ").swapcase()
+
+#print the swapped casing full name
+print(f"Your full name when casing is swapped: {name}")
