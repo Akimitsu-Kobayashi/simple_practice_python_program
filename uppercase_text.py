1e3b638884044067a5a02c33e4a2dd8f3158eb10
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-21 14:30:50 +0800

    made the string uppercase the inputted string

---
+++ uppercase_text.py
@@ -0,0 +1,7 @@
+#Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
+#uppercase the string inputted 
+name = input("Enter your full name: ").upper()
+
+#print the formatted full name
+print(f"Your full name Capitalized is: {name}")
