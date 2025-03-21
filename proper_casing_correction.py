9ab3dcb18c7890093777443c1800827409aaa23c
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-21 14:40:06 +0800

    casing correction

---
+++ proper_casing_correction.py
@@ -0,0 +1,7 @@
+#Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+
+#corrects the casing of string
+name = input("Enter your full name: ").title()
+
+# print the formatted full name
+print(f"Your full name is: {name}")
